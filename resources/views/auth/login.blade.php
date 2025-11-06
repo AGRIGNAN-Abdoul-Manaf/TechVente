c804734ca25c9c36ded3120ded12289782092231
AGRIGNAN Abdou Manaf <agrignanmanaf6@gmail.com>
2025-11-06 15:13:12 +0000

    last commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -51,7 +51,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Connexion - TechVente</title>
+    <title>Connexions - TechVente</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="min-h-screen bg-gradient-to-br from-blue-900 via-blue-700 to-blue-500 flex items-center justify-center">
